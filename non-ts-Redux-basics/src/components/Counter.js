d72f8cdf528c82c6f23458f28e76b2ba93710b7c
kara <ssctonioloriccardo@gmail.com>
2022-09-12 16:24:52 +0200

    Finished learning the basics of Redux.

--- non-ts-Redux-basics/src/components/Counter.js
+++ non-ts-Redux-basics/src/components/Counter.js
@@ -1,15 +1,41 @@
 import classes from './Counter.module.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { counterActions } from '../store/counter';
+//Usiamo useSelector e non useStore perchè ci permette di selezionare una parte particolare dello store.
 
 const Counter = () => {
-  const toggleCounterHandler = () => {};
+    const dispatch = useDispatch();
+    const counter = useSelector((state) => state.counter.counter);
+    //Automaticamente, quando il componente viene montato, esso riceve lo stato più recente
+    //dell'attributo selezionato.
+    const show = useSelector((state) => state.counter.showCounter);
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+    const incrementHandler = () => {
+        dispatch(counterActions.increment());
+    };
+    const decrementHandler = () => {
+        dispatch(counterActions.decrement());
+    };
+    const increaseHandler = () => {
+        dispatch(counterActions.increase(5));
+    };
+
+    const toggleCounterHandler = () => {
+        dispatch(counterActions.toggleCounter());
+    };
+
+    return (
+        <main className={classes.counter}>
+            <h1>Redux Counter</h1>
+            {show && <div className={classes.value}>{counter}</div>}
+            <div>
+                <button onClick={incrementHandler}>Increment</button>
+                <button onClick={increaseHandler}>Increase by 5</button>
+                <button onClick={decrementHandler}>Decrement</button>
+            </div>
+            <button onClick={toggleCounterHandler}>Toggle Counter</button>
+        </main>
+    );
 };
 
 export default Counter;
